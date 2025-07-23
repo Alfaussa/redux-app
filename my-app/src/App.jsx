f77beefd7cf01be670220d7d48204a1770c596eb
Alfiya <al.yausheva@gmail.com>
2025-07-23 15:21:19 +0300

    ⊗jsrxPmTlsIRt

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,12 @@
-function App() {
-	return <h2>This is my first Redux 
-		app!</h2> 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './app/root'
+
+
+const router = createBrowserRouter([{ path: '/', 
+	element: <Root /> }]) 
+
+  function App() {
+	return <RouterProvider router={router} />
 }
 
 export default App
